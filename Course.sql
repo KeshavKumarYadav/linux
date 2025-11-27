95defe92d5e80671e988d7f0ef691418b7519fdb
KeshavKumarYadav <37393680+KeshavKumarYadav@users.noreply.github.com>
2025-11-27 15:41:16 +0530

    Add files via upload

---
+++ Course.sql
@@ -0,0 +1,10 @@
+create table Course (
+	course_id INT,
+	course_name VARCHAR(5),
+	course_duration INT
+);
+insert into Course (course_id, course_name, course_duration) values (1, 'BTech', 4);
+insert into Course (course_id, course_name, course_duration) values (2, 'BSc', 4);
+insert into Course (course_id, course_name, course_duration) values (3, 'BCom', 4);
+insert into Course (course_id, course_name, course_duration) values (4, 'BA', 4);
+insert into Course (course_id, course_name, course_duration) values (5, 'BE', 4);
---
+++ Department.sql
@@ -0,0 +1,18 @@
+create table Department (
+	department_id INT,
+	department_name VARCHAR(11),
+	course_id INT
+);
+insert into Department (department_id, department_name, course_id) values (1, 'CSE', 1);
+insert into Department (department_id, department_name, course_id) values (2, 'IT', 1);
+insert into Department (department_id, department_name, course_id) values (3, 'ME', 5);
+insert into Department (department_id, department_name, course_id) values (4, 'ECE', 1);
+insert into Department (department_id, department_name, course_id) values (5, 'EE', 5);
+insert into Department (department_id, department_name, course_id) values (6, 'History', 4);
+insert into Department (department_id, department_name, course_id) values (7, 'English', 4);
+insert into Department (department_id, department_name, course_id) values (8, 'Physics', 2);
+insert into Department (department_id, department_name, course_id) values (9, 'Chemistry', 2);
+insert into Department (department_id, department_name, course_id) values (10, 'Mathematics', 2);
+insert into Department (department_id, department_name, course_id) values (11, 'Accounts', 3);
+insert into Department (department_id, department_name, course_id) values (12, 'Economics', 3);
+insert into Department (department_id, department_name, course_id) values (13, 'Hindi', 4);
